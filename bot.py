aeb6a9911def198829c6e439d0e651abd81d653b
kumarabhay8832-droid <kumarabhay8832@gmail.com>
2026-05-01 13:55:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters, ContextTypes
 
 # ✅ TOKEN (Railway se aayega)
-TOKEN = "8618508924:AAFB18IXWHGDJlkVjTEZYPIlTCVysiN9TRw"
+import os
+TOKEN = os.getenv("8618508924:AAFB18IXWHGDJlkVjTEZYPIlTCVysiN9TRw")
 
 # 📦 Data
 users_gender = {}
